93804141eb0adacc28173eb6cafcd30356110c3f
Nags <nagpichikaganesh@gmail.com>
2025-05-21 01:29:28 +0530

    deploymentError

--- vite.config.ts
+++ vite.config.ts
@@ -32,7 +32,7 @@ export default defineConfig(({ mode }) => ({
   },
   build: {
     target: 'esnext',
-    minify: 'terser',
+    minify: 'esbuild',
     sourcemap: false,
     rollupOptions: {
       output: {
